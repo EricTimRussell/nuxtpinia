6092c7924af871ecb1a95e1f4051f41e0456d462
Eric Russell <Erictim.Russell@live.com>
2023-01-25 13:07:10 -0700

    cart page and data

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">Your Cart</h2>
+    <div class="card text-xl" v-if="cartStore.cart.length">
+      <!--cart product list  -->
+      <div v-for="product in cartStore.cart" class="flex gap-8 items-center border-solid border-2 my-2">
+        <img :src="product.img" :alt="product.title">
+        <p class="text-white">{{ product.title }}</p>
+        <p class="text-white">{{ product.price * product.quantity }} coins</p>
+      </div>
+    </div>
+    <div v-else>
+      <p class="text-white">No Items in Cart</p>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import { useCartStore } from "@/stores/cartStore";
+const cartStore = useCartStore()
 </script>
 
 <style scoped>
