a88823442477333fedebc210b53256d4c9ae42cd
Eric Russell <Erictim.Russell@live.com>
2023-01-25 11:49:22 -0700

    Init push tailwind/nuxt setup

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=VT323&display=swap'
+        }
+      ]
+    }
+  }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#270A4B',
+        secondary: '#E8C36D',
+        dark: '#110734'
+      },
+      fontFamily: {
+        sans: ['VT323']
+      }
+    },
+  },
+  plugins: [],
+}
